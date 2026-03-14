a0b359da3a500b6f9b52ec995230dc3a4094c4c7
JRRL157 <jrrlino157@gmail.com>
2026-03-14 09:09:34 -0300

    [Rust] Library - Updates

--- scripts/src/lib.rs
+++ scripts/src/lib.rs
@@ -13,6 +13,14 @@ struct Task {
     xp: u32,
 }
 
+impl TaskRegistry {
+    pub fn load(path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let data = std::fs::read_to_string(path)?;
+        let registry: TaskRegistry = serde_json::from_str(&data)?;
+        Ok(registry)
+    }
+}
+
 struct ClaimCommand {
     task_id: String,
     repository: String,
@@ -31,8 +39,8 @@ impl TaskRegistry {
         repo: &str,
         commit_sha: &str,
     ) -> Result<bool, Box<dyn std::error::Error>> {
-        let token = env::var("PRIVATE_REPO_TOKEN")
-            .expect("PRIVATE_REPO_TOKEN still not SET!, Please fix it!");
+        let token = env::var(format!("{}_REPO_TOKEN", repo.to_uppercase()))
+            .expect(format!("{}_REPO_TOKEN still not SET!, Please fix it!", repo.to_uppercase()));
 
         let url = format!("https://api.github.com/repos/{}/{}/commits/{}", org, repo, commit_sha);
         let client = reqwest::Client::new();
@@ -58,7 +66,10 @@ impl TaskRegistry {
         sha: &str, 
         repository: &str,
         )-> Result<bool, Box<dyn std::error::Error>> {
-        let token = env::var("PRIVATE_REPO_TOKEN")?;
+        let env_var = format!("{}_REPO_TOKEN", repository.to_uppercase());
+        let token = env::var(&env_var)
+            .map_err(|_| format!("Variable {} not found!, env_var"))?;
+
         let url = format!("https://api.github.com/repos/{}/{}/compare/main...{}", org, repository, sha);
 
         let client = reqwest::Client::new();
@@ -67,7 +78,11 @@ impl TaskRegistry {
             .header(USER_AGENT, "TaskBot")
             .send()
             .await?;
-
+        
+        if !res.status().is_success() {
+            println!("❌ Erro na API do GitHub: Status {}", res.status());
+            return Ok(false);
+        }
         let json: serde_json::Value = res.json().await?;
         let status = json["status"].as_str().unwrap_or("");
         Ok(status == "behind" || status == "identical")
@@ -76,10 +91,14 @@ impl TaskRegistry {
 fn parse_claim_command(comment: &str) -> Option<ClaimCommand> {
     // valid format: /claim <Task_ID> repository:<repo_name> commit:<commit_HASH>
     let parts: Vec<&str> = comment.split_whitespace().collect();
-    if parts.len() == 3 && parts[0] == "/claim" && parts[2].starts_with("commit:") {
+    if parts.len() == 4 && parts[0] == "/claim" 
+        && parts[2].starts_with("repository:") 
+        && parts[3].starts_with("commit:") {
+        
         let quest_id = parts[1].to_string();
-        let commit_sha = parts[2].replace("commit:", "");
-        Some(ClaimCommand { quest_id, commit_sha })
+        let repo = parts[2].replace("repository:", "");
+        let commit_sha = parts[3].replace("commit:", "");
+        Some(ClaimCommand { quest_id, repo, commit_sha })
     } else {
         None
     }
